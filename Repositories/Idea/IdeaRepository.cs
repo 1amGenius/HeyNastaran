67cc79ed5a2c10546f0f134b6c7738449aed7291
Armin Pakdaman <miakoroush@gmail.com>
2025-11-09 22:46:19 +0330

    chore: added the needed functions for repositories (no logic yet!)

--- Repositories/Idea/IdeaRepository.cs
+++ Repositories/Idea/IdeaRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace Nastaran_bot.Repositories.Idea
+﻿using System.Linq.Expressions;
+
+using Nastaran_bot.Models;
+
+namespace Nastaran_bot.Repositories.Idea
 {
     public class IdeaRepository : IIdeaRepository
     {
+        public Task CreateAsync(Ideas entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Ideas>> FindAsync(Expression<Func<Ideas, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Ideas>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Ideas> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Ideas entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repositories/User/UserRepository.cs
+++ Repositories/User/UserRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace Nastaran_bot.Repositories.User
+﻿using System.Linq.Expressions;
+
+using Nastaran_bot.Models;
+
+namespace Nastaran_bot.Repositories.User
 {
     public class UserRepository : IUserRepository
     {
+        public Task CreateAsync(Users entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Users>> FindAsync(Expression<Func<Users, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Users>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Users> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Users entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
