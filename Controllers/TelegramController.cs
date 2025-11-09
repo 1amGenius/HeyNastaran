9da8399f65c6940d89069b0e6d36c4018f270eeb
Armin Pakdaman <miakoroush@gmail.com>
2025-11-09 23:50:28 +0330

    chore: depedency inject the TelegrsamBotService into TelegramController

--- Controllers/TelegramController.cs
+++ Controllers/TelegramController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Nastaran_bot.Services.TelegramBot;
+
 namespace Nastaran_bot.Controllers;
 
-public class TelegramController : ControllerBase
+public class TelegramController(TelegramBotService telegramBotService) : ControllerBase
 {
+    private readonly TelegramBotService _telegramBotService = telegramBotService;
 }
