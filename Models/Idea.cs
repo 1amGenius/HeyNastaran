b840118641f8324f91dd78109f6fb6b540170482
Armin Pakdaman <miakoroush@gmail.com>
2025-11-12 12:51:41 +0330

    chore(idea): Correct the name from Ideas to Idea

---
+++ Models/Idea.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nastaran_bot.Models;
+
+public class Idea
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id 
+    {
+        get;
+        set;
+    } = string.Empty;
+
+    [BsonElement("telegramId")]
+    public long TelegramId
+    {
+        get; set;
+    }
+
+    [BsonElement("label")]
+    public string Label 
+    {
+        get;
+        set;
+    } = "song_idea";
+
+    [BsonElement("content")]
+    public string Content 
+    {
+        get;
+        set; 
+    } = string.Empty;
+
+    [BsonElement("tags")]
+    public List<string> Tags 
+    {
+        get;
+        set;
+    } = [];
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt 
+    {
+        get;
+        set;
+    } = DateTime.UtcNow;
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt 
+    { 
+        get; 
+        set; 
+    } = DateTime.UtcNow;
+
+    [BsonElement("favorite")]
+    public bool Favorite 
+    { 
+        get;
+        set; 
+    } = false;
+}
