c9fc8ab31a2df2a0ef70ff683b583f99f22876a9
Armin Pakdaman <miakoroush@gmail.com>
2025-11-12 12:24:49 +0330

    fix(dailyNote): fix the DailyNoteModes' file name and add some of the fields

---
+++ Models/DailyNote.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nastaran_bot.Models;
+
+public class DailyNote
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id 
+    {
+        get;
+        set;
+    } = string.Empty;
+
+    [BsonElement("date")]
+    public string Date 
+    { 
+        get;
+        set;
+    } = string.Empty;
+
+    [BsonElement("text")]
+    public string Text 
+    {
+        get;
+        set; 
+    } = string.Empty;
+
+    [BsonElement("category")]
+    public string Category 
+    {
+        get;
+        set;
+    } = string.Empty;
+
+    [BsonElement("author")]
+    public string Author 
+    {
+        get; 
+        set;
+    } = "unknown";
+
+    [BsonElement("usedAt")]
+    [BsonIgnoreIfNull]
+    public DateTime? UsedAt
+    {
+        get; set;
+    }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt 
+    {
+        get;
+        set;
+    } = DateTime.UtcNow;
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt 
+    {
+        get;
+        set;
+    } = DateTime.UtcNow;
+}
