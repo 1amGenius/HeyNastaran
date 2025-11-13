a5bc28c5d912f368c9ab645e9743748db38a3b8c
Armin Pakdaman <miakoroush@gmail.com>
2025-11-13 23:17:20 +0330

    feat(command): added commandRouter with temporary logic

--- Services/TelegramBot/Commands/CommandRouter.cs
+++ Services/TelegramBot/Commands/CommandRouter.cs
@@ -1,5 +1,41 @@
-﻿namespace Nastaran_bot.Services.TelegramBot.Commands;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
 
-public class CommandRouter
+namespace Nastaran_bot.Services.TelegramBot.Commands;
+
+public class CommandRouter(IEnumerable<ICommandHandler> commandHandlers, ITelegramBotClient botClient)
 {
+    private readonly IEnumerable<ICommandHandler> _commandHandlers = commandHandlers;
+    private readonly ITelegramBotClient _botClient = botClient;
+
+    public async Task RouteAsync(Update update)
+    {
+        if (update.Message == null || string.IsNullOrWhiteSpace(update.Message.Text))
+        {
+            return;
+        }
+
+        long chatId = update.Message.Chat.Id;
+        string messageText = update.Message.Text.Trim();
+
+        string[] parts = messageText
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        
+        string command = parts[0].ToLower();
+
+        ICommandHandler handler = _commandHandlers
+            .FirstOrDefault(h => h.Command
+            .Equals(command, StringComparison.OrdinalIgnoreCase));
+
+        if (handler != null)
+        {
+            await handler
+                .HandleAsync(update);
+        }
+        else
+        {
+            _ = await _botClient
+                .SendMessage(chatId, "Sorry, I don’t recognize that command 😅");
+        }
+    }
 }
