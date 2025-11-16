84fa26d7dd4988c28bc2209a64d792c521caaa68
Armin Pakdaman <miakoroush@gmail.com>
2025-11-16 23:55:54 +0330

    chore(weather): added the needed classes for getting api responses in GetCityNameByCoordinates

--- Models/Weather.cs
+++ Models/Weather.cs
@@ -229,3 +229,31 @@ public class DailyForecast
         set;
     } = "🌤";
 }
+
+public class NominatimResponse
+{
+    public NominatimAddress Address
+    {
+        get; set;
+    }
+}
+
+public class NominatimAddress
+{
+    public string City
+    {
+        get; set;
+    }
+    public string Town
+    {
+        get; set;
+    }
+    public string Village
+    {
+        get; set;
+    }
+    public string Hamlet
+    {
+        get; set;
+    }
+}
