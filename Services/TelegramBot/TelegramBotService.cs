246b9507d5b6b66df2c3c5bedc178684aaad0598
Armin Pakdaman <miakoroush@gmail.com>
2025-11-16 22:33:00 +0330

    fix(services): fix the telegramBotService to adapt with the new architecture

--- Services/TelegramBot/TelegramBotService.cs
+++ Services/TelegramBot/TelegramBotService.cs
@@ -1,6 +1,7 @@
 ﻿using Nastaran_bot.Services.DailyNote;
 using Nastaran_bot.Services.Idea;
 using Nastaran_bot.Services.Inspiration;
+using Nastaran_bot.Services.TelegramBot.Handlers.Commands;
 using Nastaran_bot.Services.TelegramBot.Routing;
 using Nastaran_bot.Services.User;
 
@@ -12,20 +13,14 @@ using Telegram.Bot.Types.ReplyMarkups;
 namespace Nastaran_bot.Services.TelegramBot;
 
 public class TelegramBotService(
-    IUserService userService,
-    IDailyNoteService dailyNoteService,
-    IIdeaService ideaService,
-    IInspirationService inspirationService,
     ITelegramBotClient botClient,
-    CommandRouter commandRouter)
+    CommandRouter commandRouter,
+    UpdateRouter updateRouter
+    )
 {
-    private readonly IUserService _userService = userService;
-    private readonly IDailyNoteService _dailyNoteService = dailyNoteService;
-    private readonly IIdeaService _ideaService = ideaService;
-    private readonly IInspirationService _inspirationService = inspirationService;
-
     private readonly ITelegramBotClient _botClient = botClient;
     private readonly CommandRouter _commandRouter = commandRouter;
+    private readonly UpdateRouter _updateRouter = updateRouter;
 
     public async Task HandleUpdateAsync(Update update)
     {
@@ -37,26 +32,21 @@ public class TelegramBotService(
         long chatId = update.Message.Chat.Id;
         string messageText = update.Message.Text;
 
-        if (!string.IsNullOrWhiteSpace(messageText) && messageText.StartsWith(char.Parse("/")))
+        if (update.Message.Location != null)
         {
-            await _commandRouter.RouteAsync(update);
+            await _updateRouter.RouteAsync(update);
+        }
+        else if (!string.IsNullOrWhiteSpace(messageText) && messageText.StartsWith("/"))
+        {
+            bool handled = await _commandRouter.RouteAsync(update);
+            if (!handled)
+            {
+                _ = await _botClient.SendMessage(chatId, "Sorry, I don’t recognize that command 😅");
+            }
         }
         else
         {
-            _ = await _botClient.SendMessage(
-                chatId,
-                "Try sending a command like <code>/note</code> 🌤",
-                parseMode: ParseMode.Html,
-                protectContent: false,
-                replyParameters: new ReplyParameters
-                {
-                    MessageId = update.Message.MessageId
-                },
-                replyMarkup: new InlineKeyboardMarkup(
-                    InlineKeyboardButton.WithUrl("Check sendMessage method", "https://core.telegram.org/bots/api#sendmessage")
-                )
-            );
+            _ = await _botClient.SendMessage(chatId, "Send a command or share your location 🌤");
         }
     }
 }
-
