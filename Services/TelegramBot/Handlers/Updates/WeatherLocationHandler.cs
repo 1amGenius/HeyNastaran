a9a5d7408d32b50491f8f33cef2fef8043e45639
Armin Pakdaman <miakoroush@gmail.com>
2025-11-24 20:30:34 +0330

    chore: update users' location in db each time they used the /weather command

--- Services/TelegramBot/Handlers/Updates/WeatherLocationHandler.cs
+++ Services/TelegramBot/Handlers/Updates/WeatherLocationHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Nastaran_bot.Services.TelegramBot.Interfaces;
+using Nastaran_bot.Services.User;
 using Nastaran_bot.Utils.Helpers.Weather.Interfaces;
 
 using Telegram.Bot;
@@ -11,15 +12,17 @@ namespace Nastaran_bot.Services.TelegramBot.Handlers.Updates;
 public class WeatherLocationHandler(
     ITelegramBotClient botClient,
     IWeatherApiClient weatherApi,
-    ILogger<WeatherLocationHandler> logger
+    ILogger<WeatherLocationHandler> logger,
+    IUserService userService
 ) : IUpdateHandler
 {
     private readonly ITelegramBotClient _botClient = botClient;
     private readonly IWeatherApiClient _weatherApi = weatherApi;
     private readonly ILogger<WeatherLocationHandler> _logger = logger;
+    private readonly IUserService _userService = userService;
 
     public bool CanHandle(Update update)
-            => update.Message?.Location != null;
+        => update.Message?.Location != null;
 
     public async Task HandleAsync(Update update)
     {
@@ -30,14 +33,35 @@ public class WeatherLocationHandler(
                 return;
             }
 
+            long telegramId = update.Message.From.Id;
             long chatId = update.Message.Chat.Id;
+
+            Models.User user = await _userService.GetUserByTelegramIdAsync(telegramId);
+            if (user == null)
+            {
+                _ = await _botClient.SendMessage(chatId, "⚠️ Please use /start first.");
+                return;
+            }
+
             float lat = (float) update.Message.Location.Latitude;
             float lon = (float) update.Message.Location.Longitude;
+
+            string id = _userService.GetUserByTelegramIdAsync(telegramId).Result!.Id;
+            Contracts.Weather.ReverseGeocodingResult geo = await _weatherApi.GetCityAndCountryByCoordinatesAsync(lat, lon);
+
+            user = await _userService.UpdateLocationAsync(id, new()
+            {
+                Lat = lat,
+                Lon = lon,
+                City = geo.City,
+                Country = geo.Country
+            });
+
             Models.Weather weather = await _weatherApi.GetFullWeatherReportAsync(lat, lon);
             Models.CurrentWeather cw = weather.Current;
 
             string msg =
-$@"🌤 *Weather at your location*
+$@"🌤 *Weather at {user.Location.City}*
 
 🌡 Temperature: {cw.TemperatureC:F1}°C  
 🥵 Feels like: {cw.FeelsLikeC:F1}°C  
@@ -45,7 +69,7 @@ $@"🌤 *Weather at your location*
 🌬 Wind: {cw.WindSpeedKph:F1} km/h  
 ☁️ Clouds: {cw.CloudCover}%  
 🌧 Rain chance: {cw.RainChance}%  
-🔆 UV Index: {cw.UvIndex:F1}  
+🔆 UV Index: {cw.UvIndex:F1}
 
 Condition: *{cw.Condition}* {cw.Icon}
 ";
@@ -59,7 +83,7 @@ Condition: *{cw.Condition}* {cw.Icon}
             _logger.LogError(ex, "Error handling location weather");
 
             _ = await _botClient.SendMessage(
-                update.Message.Chat.Id,
+                update.Message!.Chat.Id,
                 "⚠️ Something went wrong while fetching weather."
             );
         }
