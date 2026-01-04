d982906bde0fc5a6dc2c2c8457a7f1c8e5425f0c
Armin Pakdaman <miakoroush@gmail.com>
2026-01-04 11:33:00 +0330

    chore: change the architecture in Function project to an Azure Function Application

---
+++ Function/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Core.DependencyInjection;
+
+IHost host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        IConfiguration configuration = context.Configuration;
+
+        _ = services.AddBotApplication(configuration);
+    })
+    .Build();
+
+host.Run();
---
+++ Function/TelegramWebhook.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Text.Json;
+
+using Core.Services.TelegramBot;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+using Telegram.Bot.Types;
+
+namespace Function;
+
+public class TelegramWebhook(TelegramBotService botService)
+{
+    private readonly TelegramBotService _botService = botService;
+
+    [Function("TelegramWebhook")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")]
+        HttpRequestData request,
+        FunctionContext context)
+    {
+        string body = await new StreamReader(request.Body).ReadToEndAsync();
+
+        if (string.IsNullOrWhiteSpace(body))
+        {
+            return request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        Update update;
+        try
+        {
+            update = JsonSerializer.Deserialize<Update>(body);
+        }
+        catch
+        {
+            return request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        if (update is null)
+        {
+            return request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        await _botService.HandleUpdateAsync(update, context.CancellationToken);
+
+        return request.CreateResponse(HttpStatusCode.OK);
+    }
+}
